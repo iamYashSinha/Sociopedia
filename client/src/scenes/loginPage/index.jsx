c2f87547f02badd323bfd7480cac4649bc571d2e
sinhayash9546 <sinhayash9546@gmail.com>
2023-09-19 08:35:59 +0530

    updated title

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to Socipedia, the Social App for MITIANS!
         </Typography>
         <Form />
       </Box>
